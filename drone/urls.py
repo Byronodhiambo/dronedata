47210b1cf00d9de08fa647b6e1be2f215be54d66
Byronodhiambo <byronjonnes25@gmail.com>
2022-12-06 07:54:46 +0300

    rpas list and flight urls

--- drone/urls.py
+++ drone/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('addflight/', views.addflight, name='addflight'),
     path('flights/<str:reg>/', views.flights, name='rpasflights'),
-    path('rpas/', views.rpas, name='rpasflights'),
+    path('rpas/', views.rpas, name='rpas'),
 ]
